6d3dfb8e9deb0f2a5791c965681c08c84fa8a7b0
Matias Fernandez <mifernadezs@gmail.com>
2019-09-27 12:30:10 -0300

    Commit Final 2.0

---
+++ test/cl/duoc/TestAll.java
@@ -0,0 +1,15 @@
+
+package cl.duoc;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ *
+ * @author saita
+ */
+@RunWith(Suite.class)
+@SuiteClasses({TestCrear.class, TestBuscar.class, TestEliminar.class, TestModificar.class, TestMontoBono.class})
+public class TestAll {
+    
+}
